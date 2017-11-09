bb4211b77f8e9b1520e4236d1c6e4d75fcc5ade7
Obi <obinnawalker@gmail.com>
2017-11-09 07:39:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution here!
+const drivers =  ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name){
+  return drivers.push(name)
+}
+function destructivelyPrependDriver(name){
+  drivers.unshift(name)
+}
+function destructivelyRemoveLastDriver(){
+  drivers.pop()
+}
+function destructivelyRemoveFirstDriver(){
+  drivers.shift()
+}
+function appendDriver(name){
+  let newDrivers = drivers.splice(drivers.length, 0, name)
+return newDrivers
+}
+function prependDriver(name){
+
+}
