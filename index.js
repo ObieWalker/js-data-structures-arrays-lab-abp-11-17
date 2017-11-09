8c473f688795bce8a2591b7b8c25b4e30ae6a59f
Obi <obinnawalker@gmail.com>
2017-11-09 07:44:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ function destructivelyRemoveFirstDriver(){
   drivers.shift()
 }
 function appendDriver(name){
-  let newDrivers = drivers.splice(drivers.length, 0, name)
+  let newDrivers = [...drivers]
+  newDrivers.push(name)
 return newDrivers
 }
 function prependDriver(name){
